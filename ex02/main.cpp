348a2f460a06906bf04cd9790696eb8a3bcec4a5
ys4ber <youssefsaber1337@gmail.com>
2024-09-14 15:25:10 +0100

    modify the main function

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -5,30 +5,39 @@
 #include "PresidentialPardonForm.hpp"
 
 int main()
-{
+{  
+    Bureaucrat alice("Alice", 1);
+    Bureaucrat bob("Bob", 150);
+    
+    ShrubberyCreationForm shrubbery("Garden");
+    RobotomyRequestForm robotomy("Bender");
+    PresidentialPardonForm pardon("Ford Prefect");
+    
+    std::cout << "\n-------- Signing forms with alice --------\n" << std::endl;
     try
-    {   
-        Bureaucrat alice("Alice", 1);
-        Bureaucrat bob("Bob", 150);
-        
-        ShrubberyCreationForm shrubbery("Garden");
-        RobotomyRequestForm robotomy("Bender");
-        PresidentialPardonForm pardon("Ford Prefect");
-
-        // Alice (high-ranking) will sign and execute the forms
+    {
         alice.signForm(shrubbery);
         alice.signForm(robotomy);
         alice.signForm(pardon);
-
-        std::cout << "\nExecuting forms with Alice:\n";
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    std::cout << "\n-------- Executing forms with alice --------\n" << std::endl;
+    try
+    {
         shrubbery.execute(alice);
         robotomy.execute(alice);
         pardon.execute(alice);
-
-        // Try to execute with Alice (success expected)
-    
-        // // Bob (low-ranking) will try to execute the forms (should throw exception)
-        std::cout << "\nExecuting forms with Bob:\n";
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    std::cout << "\n-------- Executing forms with bob --------\n" << std::endl;
+    try
+    {
         bob.executeForm(shrubbery);  // Should throw an exception
         bob.executeForm(robotomy);   // Should throw an exception
         bob.executeForm(pardon);     // Should throw an exception
@@ -37,5 +46,6 @@ int main()
     {
         std::cerr << e.what() << std::endl;
     }
+
     return 0;
 }
